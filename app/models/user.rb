edf7a736cf43cd3a358bca83db6613ee5dbdf323
adam1st <adamis1st@hotmail.com>
2016-03-08 22:16:09 +0000

    removed commented code in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,7 @@ class User < ActiveRecord::Base
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
           :recoverable, :rememberable, :trackable, :validatable, :omniauthable
-  #include DeviseTokenAuth::Concerns::User
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  #devise :database_authenticatable, :registerable,
-  #       :recoverable, :rememberable, :trackable, :validatable
-         
+
   def as_json(options={})
     {
       id: self.id,
