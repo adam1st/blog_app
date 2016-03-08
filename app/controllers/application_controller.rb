1877476064c66258478d1491b4bff5342dda412c
adam1st <adamis1st@hotmail.com>
2016-03-08 10:29:00 +0000

    Added mobile login and register

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
+  before_action :authenticate_user!, :except => [:index, :show]
+  acts_as_token_authentication_handler_for User, :except => [:index, :show]
+  respond_to :html, :json
+  
+   protected
+
+  def json_request?
+    request.format.json?
+  end
 end
+ 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,20 @@
 class User < ActiveRecord::Base
+  acts_as_token_authenticatable
+  # Include default devise modules.
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+  #include DeviseTokenAuth::Concerns::User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable
          
+  def as_json(options={})
+    {
+      id: self.id,
+      email: self.email
+      #auth_token: self.auth_token
+    }
+  end       
   has_many :articles
 end
